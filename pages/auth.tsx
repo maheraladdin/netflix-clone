bc125382ceadfefce241b975ec2fcc5b7943f08d
maheraladdin <73165350+maheraladdin@users.noreply.github.com>
2023-09-20 20:50:54 +0300

    UPD auth.tsx & globals.css & package.json & package-lock.json

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,6 +1,10 @@
 import Image from "next/image";
+import {useForm} from "react-hook-form";
 
 export default function Auth() {
+    const {register, handleSubmit, formState: {errors}} = useForm();
+
+
     return (
         <main>
             <div className={"relative h-screen w-screen bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-cover bg-fixed brightness-50"}>
@@ -10,10 +14,16 @@ export default function Auth() {
                 <h2 className={"text-white text-4xl mb-8 font-semibold text-center"}>
                     Sign In
                 </h2>
-                <div className={"relative flex flex-col gap-4"}>
-                    <input id={"email"} className={"sign-in-input"} type="text" placeholder={" "}/>
-                    <label htmlFor="email"> Email </label>
-                </div>
+                <form className={"relative flex flex-col gap-4"}>
+                    <div className={"relative flex flex-col gap-4"}>
+                        <input id={"email"} className={"sign-in-input"} type="email" placeholder={" "} {...register("email")}/>
+                        <label htmlFor="email"> Email </label>
+                    </div>
+                    <div className={"relative flex flex-col gap-4"}>
+                        <input id={"password"} className={"sign-in-input"} type="password" placeholder={" "} {...register("password")}/>
+                        <label htmlFor="password"> Password </label>
+                    </div>
+                </form>
             </section>
         </main>
     )
