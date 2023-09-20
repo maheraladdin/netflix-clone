39d066d74fcf0c53caf88198423d1e3d2c1f3a40
maheraladdin <73165350+maheraladdin@users.noreply.github.com>
2023-09-20 23:36:48 +0300

    UPD auth.tsx & globals.css & package.json & package-lock.json

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,28 +1,64 @@
 import Image from "next/image";
-import {useForm} from "react-hook-form";
+import {FieldErrors, useForm} from "react-hook-form";
+import * as yup from "yup";
+import {yupResolver} from "@hookform/resolvers/yup";
+import {DevTool} from "@hookform/devtools";
+
+type formValues = {
+    email: string,
+    password: string
+}
+
+const schema = yup.object({
+    email: yup.string().email().required(),
+    password: yup.string().min(8).required()
+})
 
 export default function Auth() {
-    const {register, handleSubmit, formState: {errors}} = useForm();
+    const {register, control, handleSubmit, formState: {errors}} = useForm<formValues>({
+        mode: "onChange",
+        resolver: yupResolver(schema)
+    });
+
+    const onSubmit = (data: formValues) => {
+        console.log(data);
+    }
+
+    const onError = (errors: FieldErrors<formValues>) => {
+        for (const [_, error] of Object.entries(errors)) {
+            console.log(error.message);
+        }
+    }
 
 
     return (
         <main>
-            <div className={"relative h-screen w-screen bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-cover bg-fixed brightness-50"}>
-                <Image className={"fixed top-5 left-12"} src={"/images/logo.png"} alt={"netflix logo"} width={192} height={51.9}/>
+            <DevTool control={control} placement={"top-right"}/>
+            <div className={"relative h-screen w-screen bg-black sm:bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-cover bg-fixed sm:brightness-50"}>
+                <Image className={"fixed top-5 left-5 sm:left-12 scale-50 sm:scale-100 origin-top-left"} src={"/images/logo.png"} alt={"netflix logo"} width={192} height={51.9}/>
             </div>
-            <section className={"fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-black bg-opacity-70 p-16 lg:w-2/5 lg:max-w-md rounded-md w-full"}>
-                <h2 className={"text-white text-4xl mb-8 font-semibold text-center"}>
+            <section className={"fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-black bg-opacity-70 p-16 h-screen sm:h-fit sm:w-3/5 sm:max-w-md rounded-md w-screen flex flex-col justify-center items-center"}>
+                <h2 className={"text-white text-4xl mb-8 font-semibold self-start px-5"}>
                     Sign In
                 </h2>
-                <form className={"relative flex flex-col gap-4"}>
+                <form onSubmit={handleSubmit(onSubmit, onError)} className={"relative flex flex-col gap-8"}>
                     <div className={"relative flex flex-col gap-4"}>
-                        <input id={"email"} className={"sign-in-input"} type="email" placeholder={" "} {...register("email")}/>
-                        <label htmlFor="email"> Email </label>
+                        <div className={"relative flex flex-col gap-4"}>
+                            <input id={"email"} className={"sign-in-input"} type="email" placeholder={" "} {...register("email")}/>
+                            <label htmlFor="email"> Email </label>
+                        </div>
+                        <p className={"text-red-900"}>{errors.email?.message}</p>
                     </div>
                     <div className={"relative flex flex-col gap-4"}>
-                        <input id={"password"} className={"sign-in-input"} type="password" placeholder={" "} {...register("password")}/>
-                        <label htmlFor="password"> Password </label>
+                        <div className={"relative flex flex-col gap-4"}>
+                            <input id={"password"} className={"sign-in-input"} type="password" placeholder={" "} {...register("password")}/>
+                            <label htmlFor="password"> Password </label>
+                        </div>
+                        <p className={"text-red-900"}>{errors.password?.message}</p>
                     </div>
+                    <button className={"bg-red-600 text-white rounded-md py-3"}>
+                        Sign In
+                    </button>
                 </form>
             </section>
         </main>
