fc7203fa11873b6bc8abab2fbd431b3d672fb84d
maheraladdin <73165350+maheraladdin@users.noreply.github.com>
2023-09-25 07:31:49 +0300

    UPD next.config.js

--- next.config.js
+++ next.config.js
@@ -3,7 +3,13 @@ const nextConfig = {
   reactStrictMode: true,
     images: {
         domains: ['uhdtv.io', 'upload.wikimedia.org', 'mango.blender.org', 'download.blender.org'],
-    }
+    },
+    headers: [
+        { key: "Access-Control-Allow-Credentials", value: "true" },
+        { key: "Access-Control-Allow-Origin", value: "*" },
+        { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+        { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+    ]
 }
 
 module.exports = nextConfig
