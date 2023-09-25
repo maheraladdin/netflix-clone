cbd719be5b0b8478718dbabc9e2a246fa83360bc
maheraladdin <73165350+maheraladdin@users.noreply.github.com>
2023-09-25 14:49:36 +0300

    UPD auth-form-hook.ts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,16 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import {SWRConfig} from "swr";
 import fetcher from "@/lib/fetcher";
+import {SessionProvider} from "next-auth/react";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
       <SWRConfig value={{
         fetcher,
       }}>
+        <SessionProvider session={pageProps.session}>
           <Component {...pageProps} />
+        </SessionProvider>
       </SWRConfig>
   )
 }
